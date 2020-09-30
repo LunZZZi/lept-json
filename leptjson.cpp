4886c072b546cb2a579f2104b37ed9ac81369742
baihao.wang <baihao.wang@ifchange.com>
2020-09-30 20:53:53 +0800

    feat: support number

--- leptjson.cpp
+++ leptjson.cpp
@@ -1,6 +1,7 @@
 #include "leptjson.h"
 #include <assert.h>  /* assert() */
-#include <stdlib.h>  /* NULL */
+#include <stdlib.h>  /* NULL, strtod */
+#include <cstdio>
 
 namespace leptjson {
 
@@ -44,6 +45,17 @@ static int lept_parse_false(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+static int lept_parse_number(lept_context* c, lept_value* v) {
+    char* end;
+    /* \TODO validate number */
+    v->n = strtod(c->json, &end);
+    if (c->json == end)
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json = end;
+    v->type = LEPT_NUMBER;
+    return LEPT_PARSE_OK;
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
     // check length
     switch (*c->json) {
@@ -51,7 +63,7 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
         case 't':  return lept_parse_true(c, v);
         case 'f':  return lept_parse_false(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
-        default:   return LEPT_PARSE_INVALID_VALUE;
+        default:   return lept_parse_number(c, v);
     }
 }
 
@@ -75,6 +87,11 @@ int lept_parse(lept_value* v, const char* json) {
 lept_type lept_get_type(const lept_value* v) {
     assert(v != NULL);
     return v->type;
+}
+
+double lept_get_number(lept_value* v) {
+    assert(v != NULL && v->type == LEPT_NUMBER);
+    return v->n;
 }
 
 }
--- leptjson.h
+++ leptjson.h
@@ -4,23 +4,26 @@
 namespace leptjson {
 
 typedef enum { 
-  LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT 
+  LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT
 } lept_type ;
 
 typedef struct {
+    double n;
     lept_type type;
 } lept_value;
 
 enum {
-    LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+    LEPT_PARSE_OK = 100,
+    LEPT_PARSE_EXPECT_VALUE = 200,
+    LEPT_PARSE_INVALID_VALUE = 300,
+    LEPT_PARSE_ROOT_NOT_SINGULAR = 400
 };
 
 int lept_parse(lept_value* v, const char* json);
 
 lept_type lept_get_type(const lept_value* v);
+
+double lept_get_number(lept_value* v);
 
 }
 
